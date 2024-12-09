53564c4e8b7d9db63c15bc2d0ed60539aa52336e
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 18:21:22 +0700

    delete: delete try catch

--- src/modules/cart/repository/add-cart.repository.ts
+++ src/modules/cart/repository/add-cart.repository.ts
@@ -12,37 +12,31 @@ export class AddCartRepository {
   }
 
   async addCart(addCartRequestDto: AddCartRequestDto): Promise<Cart> {
-    try {
-      const product = await this.entityManager.findOne(Product, {
-        where: { id: addCartRequestDto.productId },
-      });
-      if (!product) {
-        throw new Error('Product not found');
-      }
-
-      // Check if the cart item already exists
-      let cartItem = await this.entityManager.findOne(Cart, {
-        where: {
-          product: { id: addCartRequestDto.productId },
-          id: addCartRequestDto.cartId,
-        },
-      });
-
-      if (cartItem) {
-        cartItem.quantity += addCartRequestDto.quantity;
-        return await this.entityManager.save(Cart, cartItem);
-      }
-
-      cartItem = this.entityManager.create(Cart, {
-        product,
-        quantity: addCartRequestDto.quantity,
-        cartId: addCartRequestDto.cartId,
-      });
-
-      return await this.entityManager.save(Cart, cartItem);
-    } catch (e) {
-      this.logger.error('Error adding cart item:', e);
-      throw new Error('Error adding cart item');
+    const product = await this.entityManager.findOne(Product, {
+      where: { id: addCartRequestDto.productId },
+    });
+    if (!product) {
+      throw new Error('Product not found');
     }
+
+    let cartItem = await this.entityManager.findOne(Cart, {
+      where: {
+        product: { id: addCartRequestDto.productId },
+        id: addCartRequestDto.cartId,
+      },
+    });
+
+    if (cartItem) {
+      cartItem.quantity += addCartRequestDto.quantity;
+      return await this.entityManager.save(Cart, cartItem);
+    }
+
+    cartItem = this.entityManager.create(Cart, {
+      product,
+      quantity: addCartRequestDto.quantity,
+      cartId: addCartRequestDto.cartId,
+    });
+
+    return await this.entityManager.save(Cart, cartItem);
   }
 }

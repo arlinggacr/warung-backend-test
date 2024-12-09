07ae36a254278cf757278e96cd51dacbd7befab8
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 18:26:25 +0700

    fix: add product entity

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -1,27 +1,21 @@
 import { ConfigModule, DatabaseModule } from '@app/common';
-import { Cart } from '@app/common/entity';
+import { Cart, Product } from '@app/common/entity';
 import { UtilModule } from '@app/common/util/util.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ExploreCartRepository } from './repository/list-all-cart.repository';
-// import { SeederService } from './seeders/seeder.service';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { AddCartRepository } from './repository/add-cart.repository';
+import { ExploreCartRepository } from './repository/list-all-cart.repository';
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule,
     UtilModule,
-    TypeOrmModule.forFeature([Cart]),
+    TypeOrmModule.forFeature([Cart, Product]),
   ],
-  providers: [
-    CartService,
-    // SeederService,
-    ExploreCartRepository,
-    AddCartRepository,
-  ],
+  providers: [CartService, ExploreCartRepository, AddCartRepository],
   controllers: [CartController],
 })
 export class CartModule {}

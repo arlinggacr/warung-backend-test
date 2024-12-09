9b2c9762fa6b95151d7edba9acf6bcc8b12cf969
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 16:36:39 +0700

    feat: creating the module with importing the libs common, and then using providers in service also repository

---
+++ src/modules/warung/warung.module.ts
@@ -0,0 +1,27 @@
+import { ConfigModule, DatabaseModule } from '@app/common';
+import { Product } from '@app/common/entity';
+import { UtilModule } from '@app/common/util/util.module';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AddProductRepository } from './repository/add-product.repository';
+import { ExploreProductRepository } from './repository/list-all-products.repository';
+import { SeederService } from './seeders/seeder.service';
+import { ProductController } from './warung.controller';
+import { ProductService } from './warung.service';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    ConfigModule,
+    UtilModule,
+    TypeOrmModule.forFeature([Product]),
+  ],
+  providers: [
+    ProductService,
+    SeederService,
+    ExploreProductRepository,
+    AddProductRepository,
+  ],
+  controllers: [ProductController],
+})
+export class ProductModule {}

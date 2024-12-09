1a68bdf2bd6ccf3e9cdaaec4dbdc80f67a2ad9a3
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 16:34:16 +0700

    fix: fixing product entity into nullable true

--- libs/common/src/entity/products.entity.ts
+++ libs/common/src/entity/products.entity.ts
@@ -2,39 +2,50 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('products')
 export class Product {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'id' })
   id: number;
 
-  @Column({ type: 'int' })
+  @Column({ name: 'category_id', type: 'int', nullable: true })
   categoryId: number;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({
+    name: 'category_name',
+    type: 'varchar',
+    length: 255,
+    nullable: true,
+  })
   categoryName: string;
 
-  @Column({ type: 'varchar', length: 50, unique: true })
+  @Column({
+    name: 'sku',
+    type: 'varchar',
+    length: 50,
+    unique: true,
+    nullable: true,
+  })
   sku: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ name: 'name', type: 'varchar', length: 255, nullable: true })
   name: string;
 
-  @Column({ type: 'text', nullable: true })
+  @Column({ name: 'description', type: 'text', nullable: true })
   description: string;
 
-  @Column({ type: 'int' })
+  @Column({ name: 'weight', type: 'int', nullable: true })
   weight: number;
 
-  @Column({ type: 'int' })
+  @Column({ name: 'width', type: 'int', nullable: true })
   width: number;
 
-  @Column({ type: 'int' })
+  @Column({ name: 'length', type: 'int', nullable: true })
   length: number;
 
-  @Column({ type: 'int' })
+  @Column({ name: 'height', type: 'int', nullable: true })
   height: number;
 
-  @Column({ type: 'varchar', length: 255, nullable: true })
+  @Column({ name: 'image', type: 'varchar', length: 255, nullable: true })
   image: string;
 
-  @Column({ type: 'int' })
-  harga: number;
+  @Column({ name: 'price', type: 'int', nullable: true })
+  price: number;
 }

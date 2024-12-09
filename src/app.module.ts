7eaaf024640136c168e309d2bbb579a1286415dc
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 17:26:57 +0700

    feat: adding cart module into app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { CartModule } from './modules/cart/cart.module';
 import { ProductModule } from './modules/warung/warung.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, CartModule],
 })
 export class AppModule {}

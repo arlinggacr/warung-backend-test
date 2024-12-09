0b741a50508a0e677a91d7a84e114e9c742875c0
Arlingga Cahya Ramdhana <arlingga02@gmail.com>
2024-12-09 11:11:05 +0700

    feat: creatiung database module

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,33 @@
+import { Product } from '@app/common/entity';
+import { Module } from '@nestjs/common';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [NestConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          type: 'postgres',
+          autoLoadEntities: true,
+          entities: [Product],
+          logging: true,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
